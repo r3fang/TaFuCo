e96377e4f76571fdfb610d85c9df37ebff64881a
Rongxin Fang <r3fang@ucsd.edu>
2015-07-07 15:27:22 -0700

    add totaliter

--- foo/foo.c
+++ foo/foo.c
@@ -43,7 +43,39 @@ static PyObject *foo_FastaReader(PyObject *self, PyObject *args){
 		if (seq->qual.l) printf("qual: %s\n", seq->qual.s);
 	}
 }
+
+static PyObject *foo_totalIter(PyObject *self, PyObject *args)
+{
+	PyObject* seq;
+	PyObject* item;
+	double result = 0.0;
 	
+	/* get one argument as an iterator */
+	if(!PyArg_ParseTuple(args, "O", &seq))
+		return 0;
+	
+	seq = PyObject_GetIter(seq);
+	if (!seq)
+		return 0;
+	
+	/* process data sequentially */
+	while((item=PyIter_Next(seq))){
+		PyObject *fitem;
+		fitem = PyNumber_Float(item);
+		if(!fitem) {
+			Py_DECREF(seq);
+			Py_DECREF(item);
+			PyErr_SetString(PyExc_TypeError, "all items must be numbers");
+			return 0;
+		}
+		result += PyFloat_AS_DOUBLE(fitem);
+		Py_DECREF(fitem);
+		Py_DECREF(item);
+	}
+	Py_DECREF(seq);
+	return Py_BuildValue("d", result);
+}	
+
 /* docstring for functions */
 static char add_docs[] = 
 	"Sum of two integers.";
@@ -51,6 +83,8 @@ static char strlen_docs[] =
 	"Length of a string.";
 static char FastaReader_docs[] = 
 	"Fasta parser.";
+static char totalIter_docs[] = 
+	"Sum of a sequence of number.";
 
 static char foo_docs[] = 
 	"A collections of non-sense functions.";
@@ -60,6 +94,7 @@ static PyMethodDef foo_funcs[] = {
 	{"add", (PyCFunction)foo_add, METH_VARARGS, add_docs},
 	{"strlen", (PyCFunction)foo_strlen, METH_VARARGS, strlen_docs},
 	{"FastaReader", (PyCFunction)foo_FastaReader, METH_VARARGS, FastaReader_docs},
+	{"totalIter", (PyCFunction)foo_totalIter, METH_VARARGS, totalIter_docs},
 	{NULL}
 };
 
--- foo/test.py
+++ foo/test.py
@@ -1,2 +1,4 @@
 import foo
 print foo.FastaReader('test.seq')
+a = [1, 3, 4, 5, 6, 8]
+print foo.totalIter(a)
