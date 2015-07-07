33b931eef6e66158f7718b2acc33a51a7e5ff77d
Rongxin Fang <r3fang@ucsd.edu>
2015-07-07 14:07:05 -0700

    add strlen to foo

--- foo/foo.c
+++ foo/foo.c
@@ -10,16 +10,27 @@ static PyObject *foo_add(PyObject *self, PyObject *args){
 	return Py_BuildValue("i", a + b);
 }
 
+static PyObject *foo_strlen(PyObject *self, PyObject *args){
+	char *s;
+	/* Parsing input paramters */
+	if (!PyArg_ParseTuple(args, "s", &s)){
+		return NULL;
+	}
+	return Py_BuildValue("i", strlen(s));
+}
+
 /* docstring for functions */
 static char add_docs[] = 
 	"Sum of two integers.";
+static char strlen_docs[] = 
+	"Length of a string.";
 static char foo_docs[] = 
 	"A collections of non-sense functions.";
 
 /* Method Mapping Fucntion of the module */
 static PyMethodDef foo_funcs[] = {
-	{"add", (PyCFunction)foo_add, 
-	METH_VARARGS, add_docs},
+	{"add", (PyCFunction)foo_add, METH_VARARGS, add_docs},
+	{"strlen", (PyCFunction)foo_strlen, METH_VARARGS, strlen_docs},
 	{NULL}
 };
 
