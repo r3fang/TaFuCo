70b4de8f5e1124b74536c45e24066ab2f3440118
Rongxin Fang <r3fang@ucsd.edu>
2015-07-10 13:35:50 -0700

    delete foo

---
+++ sample_data/test.py
@@ -0,0 +1,11 @@
+import foo, itertools
+seqs = dict(foo.FastaReader('sample_data/test.seq'))
+print seqs
+seqs = dict(foo.FastaReader('sample_data/test.fa'))
+print seqs
+seq = [1, 3, 4, 5, 6, 8]
+print foo.totalIter(seq)
+print foo.totalIter(xrange(10))
+print foo.ReverseComplement('aaaattGGc')
+foo.index('sample_data/test.fa', 15)
+foo.predict('sample_data/test.fa', 'sample_data/U2OS-1-ABGHI_S29_L001_R1_001.fastq.gz')
