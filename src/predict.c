ae3407befdbf8fe3baaa737c6ae8a49e427e52f9
Rongxin Fang <r3fang@ucsd.edu>
2015-07-12 09:26:26 -0700

    @predict.c:{struct MPM(refine by adding properties of READ_NAME/EXON_NAME/READ_POS/EXON_POS/LENGTH)}

--- src/predict.c
+++ src/predict.c
@@ -16,8 +16,11 @@ KSEQ_INIT(gzFile, gzread)
  */
 struct MPM
 {
-	int read_pos;
-	char *exon_name;
+	char* READ_NAME;
+	char* EXON_NAME;	
+	int READ_POS;
+	int EXON_POS;
+	int LENGTH;
 };
 
 struct kmer_uthash *load_kmer_htable(char *fname){
@@ -97,7 +100,7 @@ struct fasta_uthash *fasta_uthash_load(char *fname){
 }
 
 char* 
-find_mpm(char *_read, int *pos_read, int k, struct kmer_uthash **kmer_ht, struct fasta_uthash **fasta_ht){
+find_mpm(char *_read, int *pos_read, int *pos_exon, int *mpm_len, int k, struct kmer_uthash **kmer_ht, struct fasta_uthash **fasta_ht){
 	/* copy a part of string */
 	char buff[k];
 	strncpy(buff, _read + *pos_read, k);
@@ -111,17 +114,17 @@ find_mpm(char *_read, int *pos_read, int k, struct kmer_uthash **kmer_ht, struct
 		*pos_read += 1;
 		return NULL;
 	}
-	int pos_seq;
+	int _pos_exon; /* tmp */
 	char *max_exon;
 	int *max_len_list = malloc(s_kmer->count * sizeof(int));
-	int max_len=0;
-	
+	int max_len = 0;
+		
 	/* discard current kmer if it matches too many MPM */
 	for(int i =0; i < s_kmer->count; i++){
 		char *tmp = strdup(s_kmer->pos[i]);
-		char* exon = pos_parser(tmp, &pos_seq);
+		char* exon = pos_parser(tmp, &_pos_exon);
 		/* error report*/
-		if(exon==NULL || pos_seq==NULL || pos_seq<0){
+		if(exon==NULL || _pos_exon==NULL || _pos_exon<0){
 			*pos_read += 1;
 			return NULL;
 		}
@@ -133,13 +136,14 @@ find_mpm(char *_read, int *pos_read, int k, struct kmer_uthash **kmer_ht, struct
 		}
 		char *_seq = strdup(s_fasta->seq);
 		int m = 0;
-		while(*(_seq + m + pos_seq) == *(_read+ *pos_read + m)){
+		while(*(_seq + m + _pos_exon) == *(_read+ *pos_read + m)){
 			m ++;
 		}
 		max_len_list[i] = m;
 		if(m > max_len){
 			max_len = m;
 			max_exon = strdup(exon);
+			*pos_exon = _pos_exon;
 		}
 		free(exon);
 		free(tmp);				
@@ -153,22 +157,23 @@ find_mpm(char *_read, int *pos_read, int k, struct kmer_uthash **kmer_ht, struct
 		return NULL;
 	}else{
 		*pos_read += max_len;
+		*mpm_len = max_len;
 		return max_exon;		
 	}
 }
 
-int find_MPMs_on_read(struct MPM *_exons[], char* _read, int k, struct kmer_uthash **kmer_ht, struct fasta_uthash **fasta_ht){
-	int pos_read = 0;
+int find_MPMs_on_read(struct MPM *_qptr, char* _read, char* _read_name, int k, struct kmer_uthash **kmer_ht, struct fasta_uthash **fasta_ht){
+	int read_pos = 0;
+	int exon_pos;
 	int i = 0; 
-	while(pos_read<(strlen(_read)-k)){
-		char* exon = find_mpm(_read, &pos_read, k, kmer_ht, fasta_ht);
+	int MPM_length;
+	while(read_pos<(strlen(_read)-k)){
+		char* exon = find_mpm(_read, &read_pos, &exon_pos, &MPM_length, k, kmer_ht, fasta_ht);
 		if (exon!=NULL){
-			//printf("exon=%s\n", exon);
-			struct MPM tmp;
-			tmp.read_pos = pos_read;	
-			tmp.exon_name = strdup(exon);	
-			//printf("%s\t%d\n", tmp.exon_name, tmp.read_pos);
-			_exons[i] = &tmp;
+			_qptr[i].READ_NAME = strdup(_read_name);
+			_qptr[i].READ_POS = read_pos;
+			_qptr[i].EXON_NAME = strdup(exon);
+			_qptr[i].LENGTH = MPM_length;
 			i++;	
 		}
 		free(exon);
@@ -191,24 +196,17 @@ int predict_main(char *fasta_file, char *fastq_file, int k){
 	seq = kseq_init(fp);
 	while ((l = kseq_read(seq)) >= 0) {
 		char *_read = strdup(seq->seq.s);
-		struct MPM **qptr = calloc(100, sizeof(struct MPM));
-		int num = find_MPMs_on_read(qptr, _read, k, &kmer_ht, &fasta_ht);		
+		char * _read_name = strdup(seq->name.s);
+		struct MPM *qptr = calloc(100, sizeof(struct MPM));
+		int num = find_MPMs_on_read(qptr, _read, _read_name, k, &kmer_ht, &fasta_ht);		
 		if(num > 0){
 			for(int i=0; i < num; i++){
-				printf("%s\t%d\n", qptr[0]->exon_name, qptr[0]->read_pos);				
+				printf("%d\t%s\t%d\t%d\n", num, qptr[0].EXON_NAME, qptr[0].READ_POS, qptr[0].LENGTH);				
 			}
 		}
 		free(qptr);
-	}
+	}	
 	
-	//struct kmer_uthash *s, *tmp;
-	//HASH_ITER(hh, kmer_ht, s, tmp) {
-	//	if(s->count > 1){
-	//		printf("%s\n", s->kmer);
-	//		for(int i=0; i<s->count; i++)
-	//		printf("%s\n", s->pos[i]);						
-	//	}
-	//}	
 	kseq_destroy(seq);
 	gzclose(fp);
 	kmer_uthash_destroy(&kmer_ht);	
