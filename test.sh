a3399fa49eb31b8af23dc87bd1d5c12795453f00
Rongxin Fang <r3fang@ucsd.edu>
2015-07-15 13:25:49 -0700

    add muscle to bin

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ gcc src/predict.c src/common.c src/fasta_uthash.c src/kmer_uthash.c src/BAG_utha
 gcc src/index.c src/common.c src/fasta_uthash.c src/kmer_uthash.c -o bin/index -lz
 
 cd sample_data
-# ../bin/index exons.fa.gz 30
+# ../bin/index exons.fa.gz 20
 ../bin/predict exons.fa.gz A431-1-ABGHI_S1_L001_R1_001.fastq.gz A431-1-ABGHI_S1_L001_R2_001.fastq.gz
