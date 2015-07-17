f654603033f5f0b9faad3a6df5cda0eb9dc94324
Rongxin Fang <r3fang@ucsd.edu>
2015-07-17 16:08:17 -0700

    find_next_MEKM change to unique kmer

--- src/predict.c
+++ src/predict.c
@@ -19,13 +19,7 @@
 #include "utils.h"
 
 /* error code */
-#define PR_ERR_NONE		     	 0 // no error
-#define PR_ERR_PARAM			-1 // bad paraemter
-#define PR_ERR_MALLOC			-2 // fail to malloc memory uthash
-#define PR_ERR_POSPARSE			-3
-#define PR_ERR_FINDHASH			-4
-#define PR_ERR_UNDEFINED		-100 // fail to malloc memory uthash
-#define MAX_READ_LEN			500
+#define PR_ERR_NONE		     	 0 		// no error
 /*--------------------------------------------------------------------*/
 /*Global paramters.*/
 static const char *pcPgmName			="predict.c";
@@ -36,23 +30,13 @@ static struct BAG_uthash *BAG_HT 		= NULL;
 
 /* Find next Maximal Extended Kmer Match (MEKM) on _read at pos_read. */
 int
-find_next_MEKM(char **exon, char *_read, int pos_read, int k, int min_match){
+find_next_MEKM(char **exon, char *_read, int pos_read, int k){
 	/*------------------------------------------------------------*/
-	/* check parameters */
-	*exon = NULL;
-	if(*exon != NULL || _read == NULL) die("find_next_MEKM: parameter error\n");	
+	/* check parameters and force *exon be NULL */
+	*exon = NULL; if(*exon != NULL || _read == NULL) die("find_next_MEKM: parameter error\n");	
 	/*------------------------------------------------------------*/
-	// parameters decleration
-	typedef struct freq{
-		int LEN;
-		size_t SIZE;
-	    UT_hash_handle hh;         /* makes this structure hashable */
-	} freq; 
-	freq* match_lens = NULL; freq *s_freq = NULL; freq *tmp = NULL;	
-	char* buff = NULL; char *exon_tmp = NULL; char *exon_max = NULL; char **matches = NULL;
-	int num = 0; int max_len = -10; int error;
-	struct fasta_uthash *s_fasta = NULL;
-	struct kmer_uthash *s_kmer = NULL;
+	char* buff = NULL; int error;
+	struct kmer_uthash *s_kmer = NULL;   //CAN'T BE FREED !!
 	/*------------------------------------------------------------*/
 	/* copy a kmer of string */
 	if((buff = malloc((k+1) * sizeof(char)))==NULL) die("find_next_MEKM: malloc fails\n");
@@ -60,39 +44,14 @@ find_next_MEKM(char **exon, char *_read, int pos_read, int k, int min_match){
 	if(buff == NULL || strlen(buff) != k) die("find_next_MEKM: buff strncpy fails\n");
 	/*------------------------------------------------------------*/
 	if((error = find_kmer(KMER_HT, buff, &s_kmer)) != PR_ERR_NONE) die("find_next_MEKM: find_kmer fails\n");
-	if(s_kmer == NULL) goto SUCCESS; // kmer not in table
+	if(s_kmer == NULL) {(*exon)=NULL; goto NO_MATCH;} // kmer not in table but not an error
 	/*------------------------------------------------------------*/
-	if((matches = malloc(s_kmer->count * sizeof(char*)))==NULL) die("find_next_MEKM: malloc fails\n");	
-	/*------------------------------------------------------------*/
-	/*
-	 * iterate all kmer matches and extend to max length
-	 */
-	//int i; for(i=0; i<s_kmer->count; i++){		
-	//	int _pos_exon; exon_tmp = pos_parser(s_kmer->pos[i], &_pos_exon);
-	//	if(exon_tmp == NULL || _pos_exon < 0) die("find_next_MEKM: pos_parser fails\n");
-	//	if((error = find_fasta(FASTA_HT, exon_tmp, &s_fasta)) != PR_ERR_NONE) die("find_next_MEKM: find_fasta fails\n");
-	//	if(s_fasta == NULL || s_fasta->seq == NULL || strlen(s_fasta->seq) < min_match) continue; //skip this match
-	//	int m = 0; while(*(s_fasta->seq + m + _pos_exon) == *(_read+ pos_read + m)){m ++;}
-	//	if(m >= min_match){
-	//		if(m>max_len){max_len=m; exon_max = strdup(exon_tmp);} // update max exon
-	//		HASH_FIND_INT(match_lens, &m, s_freq);	
-	//		// a new element
-	//		if(s_freq==NULL){
-	//			if((s_freq=(freq*)malloc(sizeof(freq))) == NULL) die("find_next_MEKM: malloc fails\n");
-	//			s_freq->LEN  = m;
-	//			s_freq->SIZE = 1;				
-	//			HASH_ADD_INT(match_lens, LEN, s_freq);
-	//		}else{
-	//			s_freq->SIZE++;
-	//		}
-	//	}
-	//}
+	if(s_kmer->count == 1){
+		int _pos_exon; (*exon) = pos_parser(s_kmer->pos[0], &_pos_exon);
+		if((*exon) == NULL || _pos_exon < 0) die("find_next_MEKM: pos_parser fails\n");
+		goto SUCCESS;
+	}else{(*exon) = NULL; goto MANY_MATCH;}	
 	/*------------------------------------------------------------*/	
-	HASH_FIND_INT(match_lens, &max_len, s_freq);	
-	if(s_freq == NULL) goto NO_MATCH; 
-	//if(s_freq->SIZE==1){(*exon) = strdup(exon_max); goto SUCCESS;}
-	if(s_freq->SIZE==1){(*exon) = NULL; goto SUCCESS;}
-	if(s_freq->SIZE>1){(*exon) = NULL; goto MANY_MATCH;}	
 	SUCCESS:
 		error = PR_ERR_NONE;
 		goto EXIT;
@@ -102,16 +61,8 @@ find_next_MEKM(char **exon, char *_read, int pos_read, int k, int min_match){
 	MANY_MATCH:
 		error = PR_ERR_NONE;
 		goto EXIT;
-		
 	EXIT:		
-		HASH_ITER(hh, match_lens, s_freq, tmp) {
-			HASH_DEL(match_lens, s_freq);
-  			free(s_freq);
-		}
 		if(buff) 		free(buff);
-		if(exon_max) 	free(exon_max);	
-		if(exon_tmp) 	free(exon_tmp);	
-		if(matches)		free(matches);
 		return error;
 }
 /*--------------------------------------------------------------------*/
@@ -121,7 +72,7 @@ int
 find_all_MEKMs(char **hits, int *num, char* _read, int _k, int min_match){
 /*--------------------------------------------------------------------*/
 	/* check parameters */
-	if(_read == NULL || hits == NULL || _k < 0 || min_match < 0) die("find_all_MEKMs: parameter error\n");
+	if(_read == NULL || hits == NULL || _k < 0) die("find_all_MEKMs: parameter error\n");
 /*--------------------------------------------------------------------*/
 	/* declare vaiables */
 	int error, _read_pos = 0;
@@ -129,7 +80,7 @@ find_all_MEKMs(char **hits, int *num, char* _read, int _k, int min_match){
 	char* _exon = NULL;
 /*--------------------------------------------------------------------*/
 	while(_read_pos<(strlen(_read)-_k-1)){
-		if((error=find_next_MEKM(&_exon, _read, _read_pos++, _k, min_match)) != PR_ERR_NONE) die("find_all_MEKMs: find_next_MEKM fails\n");
+		if((error=find_next_MEKM(&_exon, _read, _read_pos++, _k)) != PR_ERR_NONE) die("find_all_MEKMs: find_next_MEKM fails\n");
 		if (_exon != NULL){hits[*num] = strdup(_exon); (*num)++;}
 	}
 	if(_exon) free(_exon);
@@ -149,12 +100,10 @@ construct_BAG(char *fq_file1, char *fq_file2, int _k, int min_match, struct BAG_
 	hits_uniq1 = hits_uniq2 = hits1 = hits2 = parts1 = parts2 = NULL;
 	_read1 = _read2 = edge_name = gene1 = gene2 = NULL;
 	
-	if((hits1 = malloc(MAX_READ_LEN * sizeof(char*))) == NULL) die("construct_BAG: malloc error\n");
-	if((hits2 = malloc(MAX_READ_LEN * sizeof(char*))) == NULL) die("construct_BAG: malloc error\n");
-	if((hits_uniq1 = malloc(MAX_READ_LEN * sizeof(char*)))==NULL) die("construct_BAG: malloc error\n");
-	if((hits_uniq2 = malloc(MAX_READ_LEN * sizeof(char*)))==NULL) die("construct_BAG: malloc error\n");
-	if((parts1 = calloc(3, sizeof(char *))) == NULL) die("construct_BAG: malloc error\n");
-	if((parts2 = calloc(3, sizeof(char *))) == NULL) die("construct_BAG: malloc error\n");					
+	//if((hits_uniq1 = malloc(MAX_READ_LEN * sizeof(char*)))==NULL) die("construct_BAG: malloc error\n");
+	//if((hits_uniq2 = malloc(MAX_READ_LEN * sizeof(char*)))==NULL) die("construct_BAG: malloc error\n");
+	//if((parts1 = calloc(3, sizeof(char *))) == NULL) die("construct_BAG: malloc error\n");
+	//if((parts2 = calloc(3, sizeof(char *))) == NULL) die("construct_BAG: malloc error\n");					
 	
 	fp1 = gzopen(fq_file1, "r");
 	fp2 = gzopen(fq_file2, "r");
@@ -164,8 +113,12 @@ construct_BAG(char *fq_file1, char *fq_file2, int _k, int min_match, struct BAG_
 	if(fp1 == NULL || fp2 == NULL || seq1 == NULL || seq2 == NULL) die("construct_BAG: fail to read fastq files\n");
 	
 	while ((l1 = kseq_read(seq1)) >= 0 && (l2 = kseq_read(seq2)) >= 0 ) {
+		
 		_read1 = rev_com(seq1->seq.s);
 		_read2 = seq2->seq.s;
+		if((hits1 = malloc(strlen(_read1) * sizeof(char*))) == NULL) die("construct_BAG: malloc error\n");
+		if((hits2 = malloc(strlen(_read2) * sizeof(char*))) == NULL) die("construct_BAG: malloc error\n");
+		
 		if(_read1 == NULL || _read2 == NULL) die("construct_BAG: fail to get _read1 and _read2\n");
 		if(strcmp(seq1->name.s, seq2->name.s) != 0) die("construct_BAG: read pair not matched\n");
 				
@@ -201,9 +154,9 @@ construct_BAG(char *fq_file1, char *fq_file2, int _k, int min_match, struct BAG_
 		//			//	die("BAG_uthash_add fails\n");					
 		//		//}
 		//}}
+		if(hits1) 		free(hits1);
+		if(hits2) 		free(hits2);
 	}
-	if(hits1) 		free(hits1);
-	if(hits2) 		free(hits2);
 	if(hits_uniq1)  free(hits_uniq1);
 	if(hits_uniq2)  free(hits_uniq2);
 	if(parts1)		free(parts1);
