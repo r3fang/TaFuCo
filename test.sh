e06c7874bc72ca72f42017757d89c3a3056c480f
Rongxin Fang <r3fang@ucsd.edu>
2015-07-20 14:11:46 -0700

    add src/GlobalJumpAligner.cpp

--- test.sh
+++ test.sh
@@ -7,3 +7,4 @@ gcc src/predict.c src/common.c  src/utils.c -o bin/predict -lz
 #../bin/index exons.fa.gz 20
 #../bin/predict exons.fa.gz A431-1-ABGHI_S1_L001_R1_001.fastq.gz A431-1-ABGHI_S1_L001_R2_001.fastq.gz 0 0
 cat results/A431.bag.fa | python bin/rmDup.py > results/A431.bag.uniq.fa
+
