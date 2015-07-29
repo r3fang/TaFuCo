342c00a6dcb8d6926e8a75c11957b0ea916d7353
Rongxin Fang <r3fang@ucsd.edu>
2015-07-28 20:39:56 -0700

    mv src/predict.c src/main.c

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 # qsub -pe threaded 20 -N predict_1 -m abe test.sh
-gcc src/predict.c src/kstring.c -o bin/predict -lz
+gcc src/main.c src/kstring.c -o bin/tfc -lz
 #gcc -g -O2 src/alignment.c src/utils.c src/kstring.c -o bin/alignment -lz
 
 cd sample_data
-../bin/predict exons.fa U2OS-1-ABGHI_S29_L001_R1_001.fastq.gz U2OS-1-ABGHI_S29_L001_R2_001.fastq.gz 10 2
+../bin/tfc exons.fa U2OS-1-ABGHI_S29_L001_R1_001.fastq.gz U2OS-1-ABGHI_S29_L001_R2_001.fastq.gz 10 2
