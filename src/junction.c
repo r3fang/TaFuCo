d4e90df44c41c560b7eda54fa0b5eba66f6b7b16
Rongxin Fang <r3fang@ucsd.edu>
2015-08-02 16:32:01 -0700

    add min_mismatch to junction.h

--- src/junction.c
+++ src/junction.c
@@ -9,23 +9,9 @@
 #include "utils.h"
 #include "junction.h"
 
-
-static inline int min_mismatch(char* str, char* pattern){
-	if(str == NULL || pattern == NULL) die("[%s] input error"); 
-	register int i, j, n;
-	char* substring;
-	int min_mis_match = INFINITY;
-	for(i=0;i<strlen(str)-strlen(pattern);i++){
-		n = 0;
-		substring = substr(str, i, strlen(pattern));
-		// count how many mismatches
-		for(j=0; j<strlen(pattern); j++){if(toupper(pattern[j]) != toupper(substring[j])){n++;}}
-		if (n < min_mis_match){min_mis_match = n;} // update min_mis_match
-	}
-	return min_mis_match;
-} 
 int main(int argc, char *argv[]) {
 	char *junction_str = "TGAAGCCTGGAGGTGAAACC";
+	char* str2 = "GATAGTCTGTTTGTTTTGAGGACTTGGAAAGTTGTTCCTATGAAGCCTGGAGGTGAAACCCTCAAAGTCCGCTACTGGCCTCGGGACAGTTGGCCCGTGG";
 	char *fq1 = "sample_data/reads_simulated_R1.fq";
 	char *fq2 = "sample_data/reads_simulated_R2.fq";
 	int mismatch = 2;
@@ -45,12 +31,13 @@ int main(int argc, char *argv[]) {
 		if(_read1 == NULL || _read2 == NULL) die("[%s] fail to get _read1 and _read2\n", __func__);
 		if(strcmp(seq1->name.s, seq2->name.s) != 0) die("[%s] read pair not matched\n", __func__);		
 		if((min_mismatch(_read1, junction_str)) <= mismatch) printf("%s\n", _read1);
-		if((min_mismatch(_read2, junction_str)) <= mismatch) printf("%s\n", _read2);		
+		
+		//if((min_mismatch(_read2, junction_str)) <= mismatch) printf("%s\n", _read2);		
 	}
 	kseq_destroy(seq1);
 	kseq_destroy(seq2);	
 	gzclose(fp1);
 	gzclose(fp2);
-	return 0; // no error raised
+	return 0;
 	
 }
--- src/junction.h
+++ src/junction.h
@@ -9,6 +9,19 @@
 #include <math.h>
 #include "utils.h"
 
-
+static inline int min_mismatch(char* str, char* pattern){
+	if(str == NULL || pattern == NULL) die("[%s] input error"); 
+	register int i, j, n;
+	char* substring;
+	int min_mis_match = INFINITY;
+	for(i=0;i<strlen(str)-strlen(pattern);i++){
+		n = 0;
+		substring = substr(str, i, strlen(pattern));
+		// count how many mismatches
+		for(j=0; j<strlen(pattern); j++){if(toupper(pattern[j]) != toupper(substring[j])){n++;}}
+		if (n < min_mis_match){min_mis_match = n;} // update min_mis_match
+	}
+	return min_mis_match;
+} 
 
 #endif
