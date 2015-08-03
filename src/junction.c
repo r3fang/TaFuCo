9d68295e76606e9d850ae91e3aec5d98e490b69b
Rongxin Fang <r3fang@ucsd.edu>
2015-08-02 21:27:38 -0700

    rm depedency of alignment.h

--- src/junction.c
+++ src/junction.c
@@ -16,16 +16,17 @@ int main(int argc, char *argv[]) {
 	char *fq1 = "sample_data/reads_simulated_R1.fq";
 	char *fq2 = "sample_data/reads_simulated_R2.fq";
 	opt_t *opt = init_opt();
+
 	int mismatch = 2;
 	gzFile fp1, fp2;
 	register int l1, l2;
 	register kseq_t *seq1, *seq2;
 	register char *_read1, *_read2;
-	register solution_t *a;
-	if((fp1 = gzopen(fq1, "r"))==NULL)  die("[%s] fail to read fastq files\n",  __func__);
-	if((fp2 = gzopen(fq2, "r"))==NULL)  die("[%s]: fail to read fastq files\n", __func__);	
-	if((seq1 = kseq_init(fp1))==NULL)   die("[%s]: fail to read fastq files\n", __func__);
-	if((seq2 = kseq_init(fp2))==NULL)   die("[%s]: fail to read fastq files\n", __func__);	
+	register solution_t *sol1, *sol2;
+	if((fp1  = gzopen(fq1, "r")) == NULL)   die("[%s] fail to read fastq files\n",  __func__);
+	if((fp2  = gzopen(fq2, "r")) == NULL)   die("[%s] fail to read fastq files\n",  __func__);	
+	if((seq1 = kseq_init(fp1))   == NULL)   die("[%s] fail to read fastq files\n",  __func__);
+	if((seq2 = kseq_init(fp2))   == NULL)   die("[%s] fail to read fastq files\n",  __func__);	
 	
 	while ((l1 = kseq_read(seq1)) >= 0 && (l2 = kseq_read(seq2)) >= 0 ) {
 		_read1 = rev_com(seq1->seq.s); // reverse complement of read1
@@ -33,13 +34,17 @@ int main(int argc, char *argv[]) {
 		if(_read1 == NULL || _read2 == NULL) die("[%s] fail to get _read1 and _read2\n", __func__);
 		if(strcmp(seq1->name.s, seq2->name.s) != 0) die("[%s] read pair not matched\n", __func__);		
 		
-		if((min_mismatch(_read1, junction_str)) <= mismatch){
-			a = align_with_no_jump(_read1, str2, opt);
-			printf("prob=%f\n%s\n%s\n", a->prob, a->s1, a->s2);
-			free(a);
+		if((min_mismatch(_read1, junction_str)) <= mismatch ){
+			sol1 = align_with_no_jump(_read1, str2, opt);
+			printf("prob=%f\n", sol1->prob);
+			free(sol1);
+		}
+		if((min_mismatch(_read2, junction_str)) <= mismatch){
+			sol2 = align_with_no_jump(_read2, str2, opt);
+			printf("prob=%f\n", sol2->prob);
+			free(sol2);
 		}
 	}
-	
 	kseq_destroy(seq1);
 	kseq_destroy(seq2);	
 	gzclose(fp1);
--- src/main.c
+++ src/main.c
@@ -24,12 +24,242 @@
 
 /* error code */
 #define PR_ERR_NONE		     	 		0 		// error code
+
 /*--------------------------------------------------------------------*/
 /*Global paramters.*/
 static struct kmer_uthash  *KMER_HT       = NULL;
 static struct fasta_uthash *FASTA_HT      = NULL;
 static struct BAG_uthash   *BAG_HT        = NULL;
 static        junction_t   *JUNCTION_HT   = NULL;
+
+static inline solution_pair_t* align_edge(struct BAG_uthash*, struct fasta_uthash *, opt_t *);
+static inline int junction_edge(solution_pair_t*, char*, junction_t**, double, double);
+static inline junction_t *junction_gen(struct BAG_uthash *, struct fasta_uthash *, opt_t *);
+static inline ref_t *ref_generate(struct fasta_uthash *, char*, char*);
+
+static inline ref_t 
+*ref_generate(struct fasta_uthash *tb, char* gname1, char* gname2){
+	if(tb==NULL || gname1==NULL || gname2==NULL) die("[%s] input error", __func__);
+	ref_t *ref = ref_init();
+	struct fasta_uthash *s, *tmp;
+	register int i = 0;	
+	register char* str_tmp;
+	// count the number of exons
+	HASH_ITER(hh, tb, s, tmp) {
+		if(strcmp(strsplit(s->name, '.')[0], gname1) == 0) ref->S1_l += 2;
+		if(strcmp(strsplit(s->name, '.')[0], gname2) == 0) ref->S2_l += 2;
+	}
+	ref->S1 = mycalloc(ref->S1_l, int);
+	ref->S2 = mycalloc(ref->S2_l, int);
+	
+	HASH_ITER(hh, tb, s, tmp) {
+		if(s!=NULL){
+			if(strcmp(strsplit(s->name, '.')[0], gname1) == 0){				
+				if(ref->s==NULL){
+					ref->s = strdup(s->seq);
+					ref->S1[i++] = 0;
+					ref->S1[i++] = strlen(ref->s);
+				}else{
+					ref->S1[i++] = strlen(ref->s);
+					str_tmp = concat(ref->s, s->seq);
+					free(ref->s); ref->s=strdup(str_tmp);
+					free(str_tmp);
+					ref->S1[i++] = strlen(ref->s);
+				}			
+			}
+		}
+	}
+	i = 0;
+	ref->J = strlen(ref->s);
+	HASH_ITER(hh, tb, s, tmp) {
+		if(s!=NULL){
+			if(strcmp(strsplit(s->name, '.')[0], gname2) == 0){				
+				if(ref->s==NULL){
+					ref->s = strdup(s->seq);
+					ref->S2[i++] = 0;
+					ref->S2[i++] = strlen(ref->s);
+				}else{
+					ref->S2[i++] = strlen(ref->s);
+					str_tmp = concat(ref->s, s->seq);
+					free(ref->s); ref->s=strdup(str_tmp);
+					free(str_tmp);
+					ref->S2[i++] = strlen(ref->s);
+				}			
+			}
+		}
+	}
+	ref->l = strlen(ref->s);
+	return ref;
+}
+
+/*
+ * generate junction sites from breakend associated graph 
+ */
+static inline junction_t 
+*junction_gen(struct BAG_uthash *tb, struct fasta_uthash *fa, opt_t *opt){
+	if(tb == NULL || fa == NULL || opt==NULL) die("[%s] input error", __func__);	
+	struct BAG_uthash *edge, *tmp_bag;
+	register int i;
+	solution_pair_t *p;
+	junction_t *cur_junction, *tmp_junction, *ret = NULL;
+	HASH_ITER(hh, tb, edge, tmp_bag){ // iterate every edge
+		fprintf(stderr, "edge=%s \n", edge->edge);
+		if(((p = align_edge(edge, fa, opt)))==NULL) return NULL;
+		if(junction_edge(p, edge->edge, &ret, opt->min_align_score, opt->min_hits) != 0) return NULL;		
+	}
+	return ret;
+}
+
+/*
+ *
+ * Align reads that support e(Vi, Vj) to the string concated by exons of 
+ * Vi and Vj.
+ *
+ */
+static inline solution_pair_t* 
+align_edge(struct BAG_uthash *eg, struct fasta_uthash *fasta_u, opt_t *opt){
+	char* gname1 = strsplit(eg->edge, '_')[0];
+	char* gname2 = strsplit(eg->edge, '_')[1];
+	ref_t *ref1 = ref_generate(fasta_u, gname1, gname2);
+	ref_t *ref2 = ref_generate(fasta_u, gname2, gname1);
+	// because we don't know the order of gene fusion
+	// therefore, we need align E1, E2 to both order 
+	// and decide the order of gene fusion based on alignment score
+	solution_pair_t *sol_pairs_r1 = NULL;
+	solution_pair_t *sol_pairs_r2 = NULL;
+	solution_pair_t *s, *tmp; 
+	register int i, j;
+	char* idx_r1, *idx_r2;
+
+	for(i=0; i<eg->weight; i++){
+		solution_t *a = align(strsplit(eg->evidence[i], '_')[0], ref1, opt);
+		solution_t *b = align(strsplit(eg->evidence[i], '_')[1], ref1, opt);
+		solution_t *c = align(strsplit(eg->evidence[i], '_')[0], ref2, opt);
+		solution_t *d = align(strsplit(eg->evidence[i], '_')[1], ref2, opt);
+		idx_r1 = idx_md5(eg->edge, a->pos, b->pos);
+		idx_r2 = idx_md5(eg->edge, c->pos, d->pos);
+		// sol_pairs_r1
+		HASH_FIND_STR(sol_pairs_r1, idx_r1, s);
+		if(s==NULL){
+			s = solution_pair_init();
+			s->idx = strdup(idx_r1);
+			s->r1 = a; s->r2 = b;
+			s->prob = a->prob * b->prob;
+			HASH_ADD_STR(sol_pairs_r1, idx, s);  /* idx: name of key field */
+		}else{ // update with higher score
+			if(s->prob < a->prob * b->prob){
+				s->prob =  a->prob * b->prob;
+				s->r1 = a; s->r2 = b;
+			}
+		}
+		// sol_pairs_r2
+		HASH_FIND_STR(sol_pairs_r2, idx_r2, s);
+		if(s==NULL){
+			s = solution_pair_init();
+			s->idx = strdup(idx_r2);
+			s->r1 = c; s->r2 = d;
+			s->prob = c->prob * d->prob;
+			HASH_ADD_STR(sol_pairs_r2, idx, s);  /* idx: name of key field */
+		}else{ // update with higher score
+			if(s->prob < c->prob * d->prob){
+				s->prob = c->prob * d->prob;
+				s->r1 = c; s->r2 = d;
+			}
+		}
+	}
+	if(idx_r1) free(idx_r1);
+	if(idx_r2) free(idx_r2);
+	/*------------------------------------------------------------------------------*/	
+	// make decision of gene order, chose the one with larger likelihood
+	register double likehood1, likehood2;
+	likehood1 = likehood2 = 0;
+    HASH_ITER(hh, sol_pairs_r1, s, tmp) {
+		likehood1 += 10*log(s->r1->prob);
+		likehood1 += 10*log(s->r2->prob);		
+    }
+    HASH_ITER(hh, sol_pairs_r2, s, tmp) {
+		likehood2 += 10*log(s->r1->prob);
+		likehood2 += 10*log(s->r2->prob);		
+    }
+	if(ref1) ref_destory(ref1);  if(ref2) ref_destory(ref2);
+	solution_pair_t * ret;
+	if(likehood1 >= likehood2){
+		ret = sol_pairs_r1;
+	}else{
+		ret = sol_pairs_r2;
+	}
+	
+	if(gname1)         free(gname1);     
+	if(gname2)         free(gname2);
+	return ret;
+}
+
+/*
+ * generate junction sites from solution_pair_t
+ */
+static inline int 
+junction_edge(solution_pair_t *p, char* name, junction_t **ret, double MIN_ALIGN_SCORE, double MIN_HITS){
+	if(p == NULL) die("[%s] input error", __func__);
+	solution_pair_t *s, *tmp;
+	junction_t *m, *n;
+	char* idx;
+    HASH_ITER(hh, p, s, tmp) {
+		// one read
+		if(s->r1->jump == true && s->r1->prob >= MIN_ALIGN_SCORE){
+			idx = idx_md5(name, s->r1->jump_start, s->r1->jump_end);
+			HASH_FIND_STR(*ret, idx, m);
+			if(m==NULL){ // this junction not in ret
+				m = junction_init();
+				m->idx = strdup(idx);
+				m->name = strdup(name);
+				m->start = s->r1->jump_start;
+				m->end = s->r1->jump_end;
+				m->hits = 1;
+				m->likehood = 10*log(s->r1->prob); 				
+				memcpy( m->s, &s->r1->s2[m->start-HALF_JUNCTION_LEN-1], HALF_JUNCTION_LEN);
+				memcpy( &m->s[HALF_JUNCTION_LEN], &s->r1->s2[m->end], HALF_JUNCTION_LEN);
+				HASH_ADD_STR(*ret, idx, m);
+			}else{
+				m->hits ++;
+				m->likehood += 10*log(s->r1->prob); 
+			}
+		}
+		// the other read
+		if(s->r2->jump == true && s->r2->prob >= MIN_ALIGN_SCORE){
+			idx = idx_md5(name, s->r2->jump_start, s->r2->jump_end);			
+			HASH_FIND_STR(*ret, idx, m);
+			if(m==NULL){ // this junction not in ret
+				m = junction_init();
+				m->idx = strdup(idx);
+				m->name = strdup(name);
+				m->start = s->r2->jump_start;
+				m->end = s->r2->jump_end;
+				m->hits = 1;
+				m->likehood = 10*log(s->r2->prob); 
+				memcpy( m->s, &s->r2->s2[m->start-HALF_JUNCTION_LEN-1], HALF_JUNCTION_LEN);
+				memcpy( &m->s[HALF_JUNCTION_LEN], &s->r2->s2[m->end], HALF_JUNCTION_LEN);
+				HASH_ADD_STR(*ret, idx, m);
+			}else{
+				m->hits ++;
+				m->likehood += 10*log(s->r2->prob); 
+			}
+		}	
+    }
+	if(idx) free(idx);
+	// delete those junctions with hits < MIN_HITS
+	HASH_ITER(hh, *ret, m, n){
+		if(m != NULL){
+			m->likehood = m->likehood/m->hits;
+			if(m->hits < MIN_HITS){
+				HASH_DEL(*ret,m);
+				free(m);
+			}
+		}
+	}
+	return 0;
+}
+
+
 
 static inline int tfc_usage(opt_t *opt){
 	fprintf(stderr, "\n");
